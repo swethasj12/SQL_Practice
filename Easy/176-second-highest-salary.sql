b6bfbd1cf517466df61f0cd7624a38546c252324
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-08-16 00:59:14 +0530

    Create 176-second-highest-salary.sql

---
+++ Easy/176-second-highest-salary.sql
@@ -0,0 +1,10 @@
+SELECT (
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee
+WHERE salary <(SELECT MAX(salary)FROM Employee);
